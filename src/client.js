2b1e3461c66c56fc5d664f22cfba5e96e70fceab
Anton Banchev <halfsoft@gmail.com>
2021-08-15 18:03:39 +0200

    code cleanup

--- src/client.js
+++ src/client.js
@@ -3,49 +3,46 @@ import FixedQueue from './misc/FixedQueue.js';
 
 class Client {
     client;
-    timers;
     subscribed;
     klines;
-    
 
     constructor() {
         this.client = new Binance().options();
-        this.timers = [];
         this.subscribed = [];
         this.klines = {};
     }
-    
+
     subscribe(symbol, interval) {
-        if (this.subscribed.find(e => e === symbol + interval))
-            return;
+        if (!this.subscribed.find(e => e === symbol + interval)) {
 
-        this.subscribed.push(symbol + interval);
-        this.client.websockets.chart(symbol, interval, (symbol, interval, chart) => {
-            if (!this.klines[symbol + interval]) {
-                this.klines[symbol + interval] = new FixedQueue(1000, []);
-            }
-            let timeDiff = Math.abs(Object.keys(chart)[0] - Object.keys(chart)[1]);
-            for (let openTime of Object.keys(chart)) {
-                let item = chart[openTime];
-                openTime = parseInt(openTime);
-                let quoteAssetVolume = parseFloat(item.volume) / ((parseFloat(item.open) + parseFloat(item.close)) / 2);
-                const frame = [
-                    openTime,
-                    item.open,
-                    item.high,
-                    item.low,
-                    item.close,
-                    item.volume,
-                    openTime + timeDiff - 1,
-                    quoteAssetVolume.toString(),
-                    100,
-                    (quoteAssetVolume / 2).toString(),
-                    (quoteAssetVolume / 2).toString(),
-                    "1.1"];
-                this.klines[symbol + interval].push(frame);
+            this.subscribed.push(symbol + interval);
+            this.client.websockets.chart(symbol, interval, (symbol, interval, chart) => {
+                if (!this.klines[symbol + interval]) {
+                    this.klines[symbol + interval] = new FixedQueue(1000, []);
+                }
+                let timeDiff = Math.abs(Object.keys(chart)[0] - Object.keys(chart)[1]);
+                for (let openTime of Object.keys(chart)) {
+                    let item = chart[openTime];
+                    openTime = parseInt(openTime);
+                    let quoteAssetVolume = parseFloat(item.volume) / ((parseFloat(item.open) + parseFloat(item.close)) / 2);
+                    const frame = [
+                        openTime,
+                        item.open,
+                        item.high,
+                        item.low,
+                        item.close,
+                        item.volume,
+                        openTime + timeDiff - 1,
+                        quoteAssetVolume.toString(),
+                        100,
+                        (quoteAssetVolume / 2).toString(),
+                        (quoteAssetVolume / 2).toString(),
+                        "1.1"];
+                    this.klines[symbol + interval].push(frame);
 
-            }
-        }, 1000);
+                }
+            }, 1000);
+        }
     }
     async getCandles(symbol, interval) {
         if (!this.klines[symbol + interval]) {
--- src/server/index.js
+++ src/server/index.js
@@ -1,17 +1,26 @@
 import express from "express";
 import cors from "cors";
 import compression from "compression";
-import getRouter from "./router/index.js";
+import { Router } from "express";
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const getServer = async (client) => {
-    const app = express()
+    const app = express();
+    const router = new Router();
 
     app.use(cors());
     app.use(compression());
     app.use(express.json());
 
-    app.use("/", getRouter(client))
+    router.get('/api/v3/klines', async (req, res) => {
+        res.send(await client.getCandles(req.query.symbol, req.query.interval));
+    });
 
+    router.use(createProxyMiddleware({
+        target: 'https://api.binance.com', changeOrigin: true
+    }));
+
+    app.use("/", router);
     return app
 }
 
