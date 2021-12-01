85dd7655597fb95a44af65aceffa605c800752f0
Benedikt Schneppe <benedikt.schneppe@gmail.com>
2021-12-01 08:44:06 +0100

    react on relevant posix SIG signals
    Signed-off-by: Benedikt Schneppe <benjamsch@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 import getServer from "./server/index.js";
 import client from './client.js'
 
-getServer(client).then(app => app.listen(8080, '0.0.0.0'));
+const server = await getServer(client).then(app => app.listen(8080, '0.0.0.0'));
+
+
+var signals = {
+    'SIGINT': 2,
+    'SIGTERM': 15
+};
+
+function shutdown(signal, value) {
+    server.close(function () {
+        console.log('server stopped by ' + signal);
+        process.exit(128 + value);
+    });
+}
+
+Object.keys(signals).forEach(function (signal) {
+    process.on(signal, function () {
+        shutdown(signal, signals[signal]);
+    });
+});
